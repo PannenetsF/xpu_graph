8e68c309ebba4bb113eb846b81596a1ce8b1db6b
yeyeye333 <yhjyyy2024@163.com>
2025-03-25 16:17:13 +0800

    fix(flash attention): flash_attention pattern matching failed     * Bug description: Changes to the bmm pattern can affect FlashAttention, causing its pattern matching to fail     * Bugfix: passes/patterns/targets/mlu/fuse_flash_attention.py     * version-specific dependency: cntoolkit-v4.0.6, cnnl-v2.0.2, cnnlextra-v2.0.1, torch_mlu_ops-v1.4.0

--- xpu_graph/passes/patterns/targets/mlu/fuse_flash_attention.py
+++ xpu_graph/passes/patterns/targets/mlu/fuse_flash_attention.py
@@ -15,6 +15,7 @@ from ...utils.check_ops import (
     get_shape,
     check_div_or_mul_op,
     check_sub_or_add_op,
+    check_view,
 )
 
 
@@ -183,7 +184,7 @@ def validate_transpose_operation(key_transpose):
 
 
 def _is_fa(node: fx.Node):
-    if node.target != "fused_bmm":
+    if node.target != "mlu_tmo_fused_bmm_replacement":
         return False, []
     softmax_node = get_actual_node(node, 0)
     if not check_softmax_op(softmax_node):
@@ -205,8 +206,8 @@ def _is_fa(node: fx.Node):
         scale_params = params
         bmm_1_node = div_input_node
 
-    if bmm_1_node.target != "fused_bmm":
-        if bmm_1_node.target != "fused_baddbmm":
+    if bmm_1_node.target != "mlu_tmo_fused_bmm_replacement":
+        if bmm_1_node.target != "mlu_tmo_fused_bmm_add_replacement":
             return False, []
         if add_params[0] != None:
             logger.warning("Flash attention pass: Too many add operations")
@@ -214,14 +215,36 @@ def _is_fa(node: fx.Node):
         add_params[0] = bmm_1_node.args[2]
         add_params[1] = True
 
+    view1_node = bmm_1_node.args[0]
+    view2_node = bmm_1_node.args[2]
+    view3_node = node.args[2]
+
+    if all(check_view(v) for v in [view1_node, view2_node, view3_node]):
+        view1_shape = view1_node.args[0].meta["tensor_meta"].shape
+        if view1_node.args[1][0] == view1_node.args[1][0] == view1_node.args[1][0] \
+            == view1_shape[0] * view1_shape[1]:
+            return True, [
+                view1_node.args[0],
+                view2_node.args[0],
+                view3_node.args[0],
+                scale_params,
+                add_params,
+                list(node.args[-1]),
+                node.args[-3],
+            ]
+    if node.args[-1] is None:
+        output_shape = [node.args[1][0], node.args[1][1], node.args[3][2]]
+    else:
+        output_shape = list(node.args[-1])
+
     return True, [
         bmm_1_node.args[0],
-        bmm_1_node.args[1],
-        node.args[1],
+        bmm_1_node.args[2],
+        node.args[2],
         scale_params,
         add_params,
-        list(node.args[4]),
-        node.args[5],
+        output_shape,
+        node.args[-3],
     ]
 
 
